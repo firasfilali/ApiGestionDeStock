b1255fa7898743fe062599e1712070f8c6001b57
firasfilali <76403765+firasfilali@users.noreply.github.com>
2023-07-05 17:32:37 +0200

    first commit

---
+++ src/main/java/com/filali/gestiodestock/ApiGestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package com.filali.gestiodestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionDeStockApplication.class, args);
+	}
+
+}
